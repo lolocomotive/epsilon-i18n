3e7dbc4ca330fae5fdcbb4390d8b5f24dcb3aae3
lolocomotive <lolocomotive181@gmail.com>
2021-05-12 14:56:24 +0200

    Added boilerplate for Programmatic Language Features

---
+++ out/extension.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+function activate(context) { }
+exports.activate = activate;
+// this method is called when your extension is deactivated
+function deactivate() { }
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
